c35dce194a74c015ba6c91312cdc743dc2f94ca6
Peter Morawiec <peter@tradesy.com>
2018-05-29 10:38:30 -0700

    abs on price calc

--- item-cost.go
+++ item-cost.go
@@ -19,14 +19,14 @@ var itemsA = []item{
 
 var itemsB = []item{
 	item{name: "shoes", quantity: -2, price: 19.95},
-	item{name: "handbag", quantity: 1, price: 39.95},
+	item{name: "handbag", quantity: 1, price: -39.95},
 	item{name: "shirt", quantity: -3, price: 29.95},
 }
 
 func total(items []item) float64 {
 	var total float64
 	for _, item := range items {
-		total += math.Abs(float64(item.quantity)) * item.price
+		total += math.Abs(float64(item.quantity) * item.price)
 	}
 	return total
 }
